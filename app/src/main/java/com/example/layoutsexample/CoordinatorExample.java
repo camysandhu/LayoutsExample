768b6256be821eaa4d7f1fb67b326a3d3bb1d26d
charanpreetkaur026 <charanpreetkaur026@gmail.com>
2019-11-29 03:01:17 -0500

    Intent

---
+++ app/src/main/java/com/example/layoutsexample/CoordinatorExample.java
@@ -0,0 +1,14 @@
+package com.example.layoutsexample;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CoordinatorExample extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_coordinator_example);
+    }
+}
--- app/src/main/java/com/example/layoutsexample/MainActivity.java
+++ app/src/main/java/com/example/layoutsexample/MainActivity.java
@@ -2,13 +2,52 @@ package com.example.layoutsexample;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+private Button scrollViewBtn;
+private Button nesBtn;
+private Button coodBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        scrollViewBtn = findViewById(R.id.scrollBtn);
+        nesBtn = findViewById(R.id.nestedScrollBtn);
+        coodBtn = findViewById(R.id.coordinatorBtn);
+
+
+        scrollViewBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v)
+            {
+                Intent mIntent = new Intent(MainActivity.this, ScrollViewExample.class);
+                startActivity(mIntent);
+            }
+        });
+
+
+        nesBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent mIntent = new Intent(MainActivity.this, NestedScrollViewExample.class);
+                startActivity(mIntent);
+
+            }
+        });
+
+        coodBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent mIntent = new Intent(MainActivity.this, CoordinatorExample.class);
+                startActivity(mIntent);
+
+            }
+        });
     }
 }
