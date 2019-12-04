a0d3b7f0201df083281653f8b8990f8a8b828b36
Komal2006 <komaldeepkr99@gmail.com>
2019-12-04 18:47:00 -0500

    some changes

--- app/src/main/java/com/example/layoutsexample/CoordinatorExample.java
+++ app/src/main/java/com/example/layoutsexample/CoordinatorExample.java
@@ -6,13 +6,43 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
 
 public class CoordinatorExample extends AppCompatActivity {
+FloatingActionButton fb;
+ImageView imageView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_coordinator_example);
+        imageView = findViewById(R.id.imageView3);
+        imageView.setEnabled(false);
+        imageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                imageView.setEnabled(true);
+            }
+        });
+
+        fb = findViewById(R.id.fbAction);
+        fb.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+
+                Snackbar.make(v,"Floation Button is Clicked",Snackbar.LENGTH_SHORT).show();
+                imageView.setEnabled(true);
+            }
+        });
         ActionBar actionBar = getSupportActionBar();
         if (actionBar != null) {
             actionBar.setDisplayHomeAsUpEnabled(true);
